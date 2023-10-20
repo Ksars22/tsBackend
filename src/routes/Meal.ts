df3726f3c69bc74b226aba0499e84c88d5161fc1
Matt Caraher <98040867+mattcar265@users.noreply.github.com>
2023-10-19 23:52:00 -0400

    finished basic post for meal plan

--- src/routes/Meal.ts
+++ src/routes/Meal.ts
@@ -5,16 +5,21 @@ const MealRouter = express.Router();
 
 MealRouter.post('/create-meal-plan', (req, res) => {
 
+  try {
+    const uid = req.body.uid;
+    const mealJson = req.body.password;
+  
+    console.log(req.body);
+    const mealPlanData = new MealPlanModel({   
+      uid: req.body.uid,
+      mealJson: req.body.mealJson
+    });
+    mealPlanData.save();
+    res.status(200);
+  } catch (error) {
+    res.status(500).json({ "error": "Internal Server Error" });
+  }
 
-  const uid = req.body.uid;
-  const mealJson = req.body.password;
-
-  console.log(req.body);
-  const mealPlanData = new MealPlanModel({
-    uid: req.body.uid,
-    mealJson: req.body.mealJson
-  });
-  mealPlanData.save();
  
 });
 
